18e0cc297a8ca85ca12aaed6513bc677356dfd69
paulr-gflcourses <paulgerald123@gmail.com>
2018-07-19 16:33:11 +0300

    dev 30

--- 3/temp.sql
+++ 3/temp.sql
@@ -98,3 +98,62 @@ SELECT point, date FROM Outcome_o) u
 LEFT JOIN Income_o ON u.date=Income_o.date
 LEFT JOIN Outcome_o ON u.date=Outcome_o.date
 
+SELECT i.point, i.date, i.inc, Outcome_o.out FROM Income_o AS i
+LEFT JOIN Outcome_o ON i.date=Outcome_o.date
+UNION
+SELECT o.point, o.date, Income_o.inc, o.out FROM Outcome_o AS o
+LEFT JOIN Income_o ON o.date=Income_o.date
+
+
+SELECT i.point, i.date, i.inc, o.out FROM Income_o AS i
+LEFT JOIN Outcome_o AS o ON i.date=o.date
+UNION
+SELECT o.point, o.date, i.inc, o.out FROM Outcome_o AS o
+LEFT JOIN Income_o AS i ON o.date=i.date
+
+
+
+
+SELECT i.point, i.date, i.inc, o.out FROM Income_o AS i
+INNER JOIN Outcome_o AS o ON i.date=o.date
+UNION
+SELECT o.point, o.date, i.inc, o.out FROM Outcome_o AS o
+INNER JOIN Income_o AS i ON o.date=i.date
+
+
+SELECT i.point, i.date, i.inc, o.out FROM Income_o AS i
+LEFT JOIN Outcome_o AS o ON i.date=o.date
+UNION
+SELECT o.point, o.date, i.inc, o.out FROM Outcome_o AS o
+RIGHT JOIN Income_o AS i ON o.date=i.date
+
+
+
+30.
+SELECT u.point, u.date, sum(o.out) Outcome, sum(i.inc) Income FROM
+(SELECT point, date FROM Income
+UNION
+SELECT point, date FROM Outcome) u
+LEFT JOIN Income AS i ON u.date=i.date AND u.point=i.point
+LEFT JOIN Outcome AS o ON u.date=o.date AND u.point=o.point
+GROUP BY u.point, u.date
+
+
+SELECT u.point, u.date, sum(o.out) Outcome, sum(i.inc) Income FROM
+(SELECT point, date FROM Income
+UNION
+SELECT point, date FROM Outcome) u
+LEFT JOIN (SELECT DISTINCT point, date, inc FROM Income) i
+ON u.date=i.date AND u.point=i.point
+LEFT JOIN (SELECT DISTINCT point, date, out FROM Outcome) o
+ON u.date=o.date AND u.point=o.point
+GROUP BY u.point, u.date
+
+
+
+SELECT * FROM Income AS i
+FULL JOIN Outcome AS o
+ON i.point=o.point AND i.date=o.date
+ORDER BY i.point, o.point, i.date, o.date
+
+
