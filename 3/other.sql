192a3dc8092ee61df01ad696177a7d77097cfe93
student <student@GFL-1465.ad.geeksforless.net>
2018-07-24 20:08:41 +0300

    other

--- 3/other.sql
+++ 3/other.sql
@@ -14,16 +14,8 @@ right join OUTCOME_O t2 ON t1.point = t2.point and t1.date = t2.date
 30. Under the assumption that receipts of money (inc) and payouts (out) can be registered any number of times a day for each collection point [i.e. the code column is the primary key], display a table with one corresponding row for each operating date of each collection point.
     Result set: point, date, total payout per day (out), total money intake per day (inc).
     Missing values are considered to be NULL.
-30. select point, date, sum(sum_out), sum(sum_inc) 
-from
-( select point, date, sum(inc) as sum_inc, null as sum_out from INCOME 
-group by point, date 
-
-union 
-
-select point, date, null as sum_inc, sum(out) as sum_out from OUTCOME 
-group by point, date ) as t 
-	group by point, date order by point
+30. select point, date, sum(sum_out), sum(sum_inc) from( select point, date, sum(inc) as sum_inc, null as sum_out from INCOME group by point, date union select point, date, null as sum_inc, sum(out)
+    as sum_out from OUTCOME group by point, date ) as t group by point, date order by point
 
 	
 	
@@ -35,3 +27,138 @@ group by point, date ) as t
 	
 https://github.com/MindTwisted/perl_course
 
+
+https://github.com/NaPrikole/PHP_all_task.git
+https://github.com/koss89/php.git
+https://github.com/mk-maxxx1387/php-tasks.git
+https://github.com/wwwmail/php.git
+https://github.com/MindTwisted/php_course.git
+-https://github.com/Yaradovhan/GFL-php.git
+-https://github.com/mysteryc/gfl_php.git
+-
+-
+https://github.com/ucfirst-ruslan/php.git
+-
+-
+-
+-
+https://github.com/cah9hk/PHP.git
+-
+https://github.com/maksshapovalov/php.git
+-
+-
+
+
+
+
+
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+[remote "origin"]
+        url = https://github.com/ucfirst-ruslan/php.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+        remote = origin
+        merge = refs/heads/master
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+[branch "master"]
+[remote "origin"]
+        url = https://paulr-gflcourses@github.com/paulr-gflcourses/php.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+        remote = origin
+        merge = refs/heads/master
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+[remote "origin"]
+        url = https://github.com/cah9hk/PHP.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+        remote = origin
+        merge = refs/heads/master
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+[remote "origin"]
+        url = https://github.com/maksshapovalov/php.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+        remote = origin
+        merge = refs/heads/master
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+[remote "origin"]
+        url = https://github.com/NaPrikole/PHP_all_task.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+        remote = origin
+        merge = refs/heads/master
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+[remote "origin"]
+        url = https://github.com/koss89/php.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+        remote = origin
+        merge = refs/heads/master
+[remote "gitlab"]
+        url = https://gitlab.com/koss89/php.git
+        fetch = +refs/heads/*:refs/remotes/gitlab/*
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+[remote "origin"]
+        url = https://github.com/mk-maxxx1387/php-tasks.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+[remote "origin"]
+        url = https://github.com/wwwmail/php.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+        remote = origin
+        merge = refs/heads/master
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+[remote "origin"]
+        url = https://github.com/MindTwisted/php_course.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+        remote = origin
+        merge = refs/heads/master
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
